dcf28f8f645835ba6928d9666019dae0111005fb
Michael Ryvlin <ryvlin@gmail.com>
2013-09-07 20:45:43 +0200

    initial commit

---
+++ app/code/community/Hackathon/PSR0Autoloader/Model/Observer.php
@@ -0,0 +1,26 @@
+<?php
+
+class Hackathon_PSR0Autoloader_Model_Observer extends Mage_Core_Model_Observer {
+
+	const CONFIG_PATH_PSR0NAMESPACES = 'global/psr0_namespaces';
+
+	protected function getNamespacesToRegister(){
+		$namespaces = array();
+		$node = Mage::getConfig()->getNode(self::CONFIG_PATH_PSR0NAMESPACES);
+		if ($node && is_array($node->asArray())){
+			$namespaces = array_keys($node->asArray());
+		}
+		return $namespaces;
+	}
+
+	public function addAutoloader() {
+		foreach ($this->getNamespacesToRegister() as $namespace){
+			if (is_dir(Mage::getBaseDir('lib') . DS . $namespace)){
+				$args = array($namespace, Mage::getBaseDir('lib') . DS . $namespace);
+				$autoloader = Mage::getModel("psr0autoloader/splAutoloader", $args);
+				$autoloader->register();
+			}
+		}
+	}
+
+}
